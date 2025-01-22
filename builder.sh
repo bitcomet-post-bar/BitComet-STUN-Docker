84b9060625ce64758d0fb20672bf48986ef73dd6
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-22 23:47:26 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -16,7 +16,6 @@ wget https://github.com/PBH-BTN/PeerBanHelper/releases/download/v${VER}/PeerBanH
 unzip PBH.zip -d /files
 
 # 生成 PeerBanHelper 的 JRE
-wget https://github.com/PBH-BTN/PeerBanHelper/releases/download/v${VER}/PeerBanHelper_Windows_${VER}_Portable.zip -O /tmp/PBH.zip
-unzip /tmp/PBH.zip -d /tmp
-DEPS=$(cat /tmp/PeerBanHelper/jre/release | grep MODULES | grep -oE '".*"' | tr -d '"' | tr ' ' ',')
+for JAR in $(find /files/PeerBanHelper | grep .jar); do jdeps $JAR >>/tmp/DEPS; done
+DEPS=$(cat /tmp/DEPS | awk '{print$NF}' | grep -E '^(java|jdk)\.' | sort | uniq | tr '\n' ',' | sed 's/,$//)
 jlink --no-header-files --no-man-pages --compress=zip-9 --strip-debug --add-modules $DEPS --output /files/PeerBanHelper/jre
