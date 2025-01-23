bef603e94fbe744991f48631d84f3c66a018dedb
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-01-23 21:56:31 +0800

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -19,8 +19,3 @@ unzip PBH.zip -d /files
 for JAR in $(find /files/PeerBanHelper | grep .jar); do jdeps --multi-release 21 $JAR >>/tmp/DEPS 2>/dev/null; done
 DEPS=$(cat /tmp/DEPS | awk '{print$NF}' | grep -E '^(java|jdk)\.' | sort | uniq | tr '\n' ',')jdk.crypto.ec
 jlink --no-header-files --no-man-pages --compress=zip-9 --strip-debug --add-modules $DEPS --output /files/PeerBanHelper/jre
-
-# 生成 Alpine Linux 镜像
-# wget https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/$ARCH/alpine-minirootfs-${ALPINE_VER}-${ARCH}.tar.gz -O alpine.tar.gz
-# mkdir -p /alpine
-# tar xzf alpine.tar.gz -C /alpine
