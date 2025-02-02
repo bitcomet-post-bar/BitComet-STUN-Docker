a7244219656a06c28eebe7481c54626f57d814a8
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-02-02 21:04:21 +0800

    Update natmap.sh

--- files/natmap.sh
+++ files/natmap.sh
@@ -9,10 +9,31 @@ OWNADDR=$6
 # 定义日志函数
 LOG() { tee -a /BitComet/DockerLogs.log ;}
 
-echo 当前穿透通道为 $WANADDR:$WANPORT，即将更新 BitComet 监听端口 | LOG
-/files/BitComet/bin/bitcometd --bt_port $WANPORT &
+echo 当前穿透通道为 $WANADDR:$WANPORT | LOG
 
-echo 开始执行 UPnP | LOG
-[ $UpnpInterface ] && UpnpInterface='-m '$UpnpInterface''
-UPNPC=$(upnpc $UpnpInterface -i -e "STUN BitComet Docker" -a @ $WANPORT $LANPORT $L4PROTO 2>&1 >/dev/null)
-[ $UPNPC ] && echo $UPNPC | LOG
+# 保存穿透信息
+touch /BitComet/DockerSTUN.log
+[ $(wc -l </hath/stun.log) -ge 1000 ] && mv /BitComet/DockerSTUN.log /BitComet/DockerSTUN.log.old
+echo [$(date)] $WANADDR:$WANPORT '->' $OWNADDR:$LANPORT '->' $WANPORT >>/BitComet/DockerSTUN.log
+echo $WANPORT $LANPORT >/BitComet/DockerSTUNPORT
+
+echo 执行 UPnP | LOG
+[ $UpnpInterface ] && export UpnpInterface='-m '$UpnpInterface''
+[ $UpnpUrl ] && export UpnpUrl='-u '$UpnpUrl''
+[ $UpnpAddr ] || export UpnpAddr=@
+echo 本次 UPnP 规则：转发 外部端口 $LANPORT 至 内部端口 $WANPORT | LOG
+UpnpStart='upnpc '$UpnpArgs' '$UpnpInterface' '$UpnpUrl' -i -e "STUN BitComet Docker" -a '$UpnpAddr' '$WANPORT' '$LANPORT' '$L4PROTO''
+echo 本次 UPnP 执行命令 | LOG
+echo $UpnpStart | LOG
+$UpnpErr=$($UpnpStart 2>&1 >/dev/null)
+[ $UpnpErr ] && echo 添加 UPnP 规则失败，错误信息如下 | LOG && echo $UpnpErr | LOG && \
+[ $UpnpInterface ] || (
+[ $(ls /sys/class/net | grep -o br-lan) ] && (
+echo 尝试使用 br-lan 接口添加 UPnP 规则 | LOG
+UpnpStart='upnpc '$UpnpArgs' -m br-lan '$UpnpUrl' -i -e "STUN BitComet Docker" -a '$UpnpAddr' '$WANPORT' '$LANPORT' '$L4PROTO''
+echo 本次 UPnP 执行命令 | LOG
+echo $UpnpStart | LOG
+eval $UpnpStart ))
+
+echo 更新 BitComet 监听端口 | LOG
+/files/BitComet/bin/bitcometd --bt_port $WANPORT &
