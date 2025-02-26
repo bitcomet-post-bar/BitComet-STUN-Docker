a09e027e41a0c9b87f8b7e051e8b9bb2af5b77a2
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-02-27 03:38:43 +0800

    Update stun.sh

--- files/stun.sh
+++ files/stun.sh
@@ -3,12 +3,6 @@
 # 初始化变量
 L4PROTO=$1
 [ $StunInterval ] || export StunInterval=25
-[ $StunInterface ] && \
-if [[ "$StunInterface" =~ ([0-9]{1,3}\.){3}[0-9]{1,3} ]]; then
-	StunInterface=',bind='$StunInterface''
-else
-	StunInterface=',interface='$StunInterface''
-fi
 
 # 定义日志函数
 LOG() { echo "$*" | tee -a /BitComet/DockerLogs.log ;}
@@ -35,32 +29,61 @@ UPDATE_SERVERS() {
 # 检测穿透通道
 GET_NAT() {
 	for SERVER in $(cat /tmp/StunServers_$L4PROTO.txt); do
-		local IP=$(echo $SERVER | awk -F : '{print$1}')
-		local PORT=$(echo $SERVER | awk -F : '{print$2}')
-		local RES=$(echo "000100002112a442$(head -c 12 /dev/urandom | xxd -p)" | xxd -r -p | eval timeout 2 socat - ${L4PROTO}4:$IP:$PORT,reuseport,sourceport=$1$StunInterface 2>&1 | xxd -p -c 64)
-		echo $RES | tr -d ' ' | grep -q 4164647265737320616c726561647920696e20757365 && {
-			let BC_BT_PORT_SHUF++
-			[ $BC_BT_PORT_SHUF -ge 10 ] && {
+		pgrep -f socat.+$L4PROTO.+$L4PROTO >/dev/null || {
+			LOG socat 监听端口 $STUN_BIND_PORT/$L4PROTO，使用 STUN 服务器 $SERVER
+			socat ${L4PROTO}4-listen:$STUN_BIND_PORT,reuseport,fork ${L4PROTO}4:$SERVER,reuseport,sourceport=$STUN_BIND_PORT$STUN_IFACE 2>/tmp/socat_$L4PROTO.txt &
+			grep -q 'Address already in use' /tmp/socat_$L4PROTO.txt && {
+				if [[ $StunMode =~ nft ]]; then
+					let STUN_PORT_FLAG++
+					[ $STUN_PORT_FLAG -ge 10 ] && LOG 监听端口失败次数达到上限，停止容器 && kill 1 && exit
+					LOG 监听端口失败，尝试使用其他端口
+					pkill -9 -f socat.+$L4PROTO.+$L4PROTO
+					STUN_BIND_PORT=$(shuf -i 1024-65535 -n 1)
+					for HANDLE in $(nft -as list chain ip STUN SNAT | grep \"$NFTNAME\" | grep $L4PROTO | awk '{print$NF}'); do nft delete rule ip STUN SNAT handle $HANDLE; done
+					nft insert rule ip STUN SNAT skuid 50080 $OIFNAME $APPRULE $L4PROTO sport $STUN_BIND_PORT counter snat to :$STUN_ORIG_PORT comment $NFTNAME
+					continue
+				else
+					LOG 监听端口失败，停止容器 && kill 1 && exit
+			}
+			STUN_PORT_FLAG=0
+		}
+		local HEX=$(echo "000100002112a442$(head -c 12 /dev/urandom | xxd -p)" | xxd -r -p | eval timeout 2 socat - ${L4PROTO}4:127.0.0.1:$STUN_BIND_PORT | xxd -p -c 0 | grep -oE '002000080001.{12}')
+		if echo $RES | tr -d ' ' | grep -q 4164647265737320616c726561647920696e20757365; then
+			let STUN_PORT_FLAG++
+			[ $STUN_PORT_FLAG -ge 10 ] && {
 				LOG 端口冲突次数达到上限，停止容器
 				kill -15 1
 				exit
 			}
-			LOG 检测到端口冲突，修改 BitComet BT 端口
-			while
-				awk '{print$2,$4}' /proc/net/tcp /proc/net/tcp6 | grep 0A | grep -qiE '(0{8}|0{32}):'$(printf '%04x' $BITCOMET_BT_PORT)'' || \
-				awk '{print$2,$4}' /proc/net/udp /proc/net/udp6 | grep 07 | grep -qiE '(0{8}|0{32}):'$(printf '%04x' $BITCOMET_BT_PORT)'' || \
-				echo $BITCOMET_BT_PORT | grep -qE '^('$BITCOMET_WEBUI_PORT'|'$PBH_WEBUI_PORT'|'$StunMitmEnPort'|'$StunMitmDePort')$'
-			do export BITCOMET_BT_PORT=$(shuf -i 10000-65535 -n 1); done
-			/files/BitComet/bin/bitcometd --bt_port $BITCOMET_BT_PORT >/dev/null
-			break
-		}
-		local HEX=$(echo $RES | grep -oE '002000080001.{12}')
+			if [[ $StunMode =~ nft ]]; then
+			export 
+			else
+				LOG 检测到端口冲突，请确保 $STUN_BIND_PORT/$L4PROTO 未被其他程序使用
+			fi
+			continue
+		else
+			STUN_PORT_FLAG=0
+		fi
 		if [ $HEX ]; then
 			[ ${HEX:12:4} = "${STUN_HEX:12:4}" ] && break
+			if [ $(($(date +%s)-$STUN_TIME)) -lt $(($StunInterval*2)) ]; then
+				let STUN_TIME_FLAG++
+				LOG 穿透通道保持时间低于 $(($StunInterval*2)) 秒（两次心跳间隔）
+				[ $STUN_TIME_FLAG -ge 10 ] && {
+					LOG 连续 10 次保持时间过短，暂停穿透 3600 秒
+					sleep 3600
+					STUN_TIME=0
+					STUN_TIME_FLAG=0
+					break
+				}
+			else
+				STUN_TIME_FLAG=0
+			fi
 			STUN_HEX=$HEX
 			STUN_IP=$(printf '%d.%d.%d.%d\n' $(printf '%x\n' $((0x${HEX:16:8}^0x2112a442)) | sed 's/../0x& /g'))
 			STUN_PORT=$((0x${HEX:12:4}^0x2112))
-			stun_exec.sh $STUN_IP $STUN_PORT $StunBindPort $L4PROTO &
+			STUN_TIME=$(date +%s)
+			stun_exec.sh $STUN_IP $STUN_PORT $STUN_BIND_PORT $L4PROTO &
 			break
 		else
 			LOG STUN 服务器 $SERVER 不可用，后续排除
@@ -69,11 +92,26 @@ GET_NAT() {
 	done
 }
 
-# 初始化
+# 初始化 STUN
 [ -s /tmp/StunServers_$L4PROTO.txt ] || sort -R StunServers.txt >/tmp/StunServers_$L4PROTO.txt
+STUN_TIME=0
+LOG 当前 STUN 心跳间隔为 $StunInterval 秒
+[ $StunInterface ] && LOG 当前 STUN 绑定接口为 $StunInterface
+if [[ $StunMode =~ nft ]]; then
+	STUN_BIND_PORT=$(shuf -i 1024-65535 -n 1)
+	NFTNAME=Docker_BitComet_$STUN_ORIG_PORT
+	[ $STUN_IFACE_IP ] && APPRULE='ip saddr '$StunInterface''
+	[ $STUN_IFACE_IP ] || APPRULE='ip saddr != 127.0.0.1'
+	[ $STUN_IFACE_IF ] && OIFNAME='oifname '$StunInterface''
+	nft add chain ip STUN SNAT { type nat hook postrouting priority srcnat + 5 \; }
+	nft insert rule ip STUN SNAT skuid 50080 $OIFNAME $APPRULE $L4PROTO sport $STUN_BIND_PORT counter snat to :$STUN_ORIG_PORT comment $NFTNAME
+else
+	STUN_BIND_PORT=$STUN_ORIG_PORT
+fi
 
+# 执行 STUN
 while :; do
 	[ -s /tmp/StunServers_$L4PROTO.txt ] || UPDATE_SERVERS
-	GET_NAT $StunBindPort
+	GET_NAT
 	sleep $StunInterval
 done
