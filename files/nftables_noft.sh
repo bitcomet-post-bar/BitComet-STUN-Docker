1ef6fbda4ac63998f47c27857123e2af9440f99c
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-02-27 03:39:34 +0800

    Update nftables_noft.sh

--- files/nftables_noft.sh
+++ files/nftables_noft.sh
@@ -6,13 +6,14 @@ pkill -Af "$0 $*"
 CTMARK=$1
 TABLE=$(nft -st list ruleset | sed '/flow add @/q' | grep -oE '^table.*\{' | awk '{print$2,$3}' | tail -1)
 CHAIN=$(nft -st list ruleset | sed '/flow add @/q' | grep -oE 'chain.*\{' | awk '{print$2}' | tail -1)
+NFTNAME=Docker_BitComet_$STUN_ORIG_PORT
 while :; do
-	nft -s list chain $TABLE $CHAIN | grep -q ${OWNNAME}_noft || {
+	nft -s list chain $TABLE $CHAIN | grep -q ${NFTNAME}_noft || {
 		HANDLE=$(nft -as list chain $TABLE $CHAIN | sed '/flow add @/q' | awk 'END{print$NF}')
 		[ $HANDLE ] && {
 			echo 检测到软件加速，绕过 Tracker 流量 | tee -a /BitComet/DockerLogs.log
-			nft insert rule $TABLE $CHAIN handle $HANDLE $OIFNAME $APPRULE tcp flags { syn, ack } accept comment ${OWNNAME}_noft
-			nft insert rule $TABLE $CHAIN handle $HANDLE $OIFNAME $APPRULE ct mark $CTMARK counter accept comment ${OWNNAME}_noft
+			nft insert rule $TABLE $CHAIN handle $HANDLE $OIFNAME $APPRULE tcp flags { syn, ack } accept comment ${NFTNAME}_noft
+			nft insert rule $TABLE $CHAIN handle $HANDLE $OIFNAME $APPRULE ct mark $CTMARK counter accept comment ${NFTNAME}_noft
 		}
 	}
 	sleep 60
