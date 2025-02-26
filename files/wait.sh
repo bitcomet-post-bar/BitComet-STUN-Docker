9795c062b8b279aa845e32e38012a4f1e14ad56f
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-02-26 20:39:36 +0800

    Delete files/wait.sh

--- files/wait.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-EXIT() {
-	echo 正在停止容器 | tee -a /BitComet/DockerLogs.log
-	kill -15 $(ps ax | awk '{print$1}' | grep -vE '^(PID|1)$') 2>/dev/null
-}
-trap EXIT SIGTERM
-sleep infinity &
-wait
