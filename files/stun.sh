99227d44d04ba0bbf9f84dbbc36097dc58529c88
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-02-28 23:39:58 +0800

    Update stun.sh

--- files/stun.sh
+++ files/stun.sh
@@ -10,22 +10,6 @@ LOG() { echo "$*" | tee -a /BitComet/DockerLogs.log ;}
 # 防止脚本重复运行
 pkill -Af "$0 $*"
 
-# 更新 HTTP 服务器
-UPDATE_HTTP() {
-	LOG 已启用 TCP 通道，更新 HTTP 服务器列表，最多等待 15 秒
-	echo -ne "GET /topsite_ip.txt HTTP/1.1\r\nHost: oniicyan.pages.dev\r\nConnection: close\r\n\r\n" | \
-	timeout 15 openssl s_client -connect oniicyan.pages.dev:443 -quiet 2>/dev/null | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' >/tmp/SiteList.txt
-	if [ -s /tmp/SiteList.txt ]; then
-		LOG 更新 HTTP 服务器列表成功
-		mv -f /tmp/SiteList.txt SiteList.txt
-	else
-		LOG 更新 HTTP 服务器列表失败，本次跳过
-		[ -f SiteList.txt ] || cp /files/SiteList.txt SiteList.txt
-	fi
-	sort -R SiteList.txt >/tmp/SiteList_tcp.txt
-	LOG 已加载 $(wc -l </tmp/SiteList_tcp.txt) 个 HTTP 服务器
-}
-
 # 更新 STUN 服务器
 UPDATE_STUN() {
 	LOG 更新 STUN 服务器列表，最多等待 15 秒
@@ -61,10 +45,10 @@ GET_NAT() {
 		unset STUN_PORT_FLAG
 		[ $HEX ] && {
 			[ ${HEX:12:4} = "${STUN_HEX:12:4}" ] && break
-			[ $(($(date +%s)-$STUN_TIME)) -lt $(($StunInterval/$STUN_TIME_FLAG*2)) ] && {
-				LOG 穿透通道保持时间低于 $(($StunInterval/$STUN_TIME_FLAG*2)) 秒（两次心跳间隔）
+			[ $(($(date +%s)-$STUN_TIME)) -lt $(($StunInterval/$STUN_TIME_FLAG*3)) ] && {
+				LOG 穿透通道保持时间低于 $(($StunInterval/$STUN_TIME_FLAG*3)) 秒（三次心跳间隔）
 				let STUN_TIME_FLAG++
-				LOG 降低 STUN 心跳间隔，当前为 $(($StunInterval/$STUN_TIME_FLAG)) 秒
+				LOG 缩短 STUN 心跳间隔，当前为 $(($StunInterval/$STUN_TIME_FLAG)) 秒
 				[ $STUN_TIME_FLAG -ge 10 ] && {
 					LOG 连续 10 次保持时间过短，暂停穿透 3600 秒
 					sleep 3600
@@ -85,18 +69,6 @@ GET_NAT() {
 	done
 }
 
-# 穿透通道保活
-KEEPALIVE() {
-	for SERVER in $(cat /tmp/SiteList_$L4PROTO.txt); do
-		local RES=$(echo -ne "HEAD / HTTP/1.1\r\nHost: $SERVER\r\nConnection: keep-alive\r\n\r\n" | eval runuser -u socat -- timeout 2 socat - tcp4:$SERVER:80,reuseport,sourceport=$STUN_BIND_PORT$STUN_IFACE 2>&1)
-		echo "$RES" | grep -q HTTP && break
-		let STUN_HTTP_FLAG++
-		sed '/^'$SERVER'$/d' -i /tmp/SiteList_$L4PROTO.txt
-		[ $STUN_HTTP_FLAG -ge 10 ] && LOG HTTP 保活连续失败 10 次，本次跳过 && break
-	done
-	unset STUN_HTTP_FLAG
-}
-
 # 初始化 STUN
 [ -s /tmp/StunServers_$L4PROTO.txt ] || sort -R StunServers.txt >/tmp/StunServers_$L4PROTO.txt
 STUN_TIME=0
@@ -104,7 +76,7 @@ STUN_TIME_FLAG=1
 LOG 当前 STUN 心跳间隔为 $StunInterval 秒
 [ $StunInterface ] && LOG 当前 STUN 绑定接口为 $StunInterface
 if [[ $StunMode =~ nft ]]; then
-	STUN_BIND_PORT=$(shuf -i 1024-65535 -n 1)
+	export STUN_BIND_PORT=$(shuf -i 1024-65535 -n 1)
 	NFTNAME=Docker_BitComet_$STUN_ORIG_PORT
 	[ $STUN_IFACE_IP ] && APPRULE='ip saddr '$StunInterface''
 	[ $STUN_IFACE_IP ] || APPRULE='fib daddr type != local'
@@ -117,10 +89,11 @@ if [[ $StunMode =~ nft ]]; then
 	for HANDLE in $(nft -as list chain ip STUN NAT_POSTROUTING | grep \"${NFTNAME}_snat\" | grep $L4PROTO | awk '{print$NF}'); do nft delete rule ip STUN NAT_POSTROUTING handle $HANDLE; done
 	nft insert rule ip STUN NAT_POSTROUTING meta l4proto $L4PROTO ct mark 0x50080 counter snat to :$STUN_ORIG_PORT comment ${NFTNAME}_snat
 else
-	STUN_BIND_PORT=$STUN_ORIG_PORT
+	export STUN_BIND_PORT=$STUN_ORIG_PORT
 fi
+[ $L4PROTO = tcp ] && ! pgrep -f stun_keep.sh >/dev/null && LOG 已启用 TCP 通道，执行 HTTP 保活 && stun_keep.sh &
 
-# 执行 STUN
+# 循环执行 STUN
 while :; do
 	[ -s /tmp/StunServers_$L4PROTO.txt ] || UPDATE_STUN
 	[ $L4PROTO = tcp ] && [ ! -s /tmp/SiteList_tcp.txt ] && UPDATE_HTTP
