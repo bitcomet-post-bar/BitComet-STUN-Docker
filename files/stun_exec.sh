c5baef9b1b70c776a56c1a7ab19f94c987d8bb90
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-03-01 15:19:31 +0800

    Update stun_exec.sh

--- files/stun_exec.sh
+++ files/stun_exec.sh
@@ -14,7 +14,7 @@ LOG() { echo "$*" | tee -a /BitComet/DockerLogs.log ;}
 LOG 本次穿透通道为 $WANADDR:$WANPORT/$L4PROTO
 
 # 防止脚本重复运行
-pkill -Af "$0 $*"
+pkill -Af $0.*$L4PROTO
 
 # 初始化穿透信息
 touch /BitComet/DockerStun.log
@@ -97,16 +97,21 @@ echo $WANPORT $LANPORT >StunPort_$L4PROTO
 	[ $UPNP_FLAG = 2 ] && LOG 更新 UPnP 规则失败，错误信息如下 && LOG "$UPNP_RES" | tail -1
 }
 
-# 连通性检测
-if echo | socat - $L4PROTO:$WANADDR:$WANPORT,connect-timeout=2 2>/dev/null; then
-	LOG $WANADDR:$WANPORT/$L4PROTO 连通性检测成功
-else
-	LOG $WANADDR:$WANPORT/$L4PROTO 连通性检测失败，请确认路径上的防火墙
-fi
-
 # TCP 通道保活
 [ $L4PROTO = tcp ] && ! pgrep -f stun_keep.sh >/dev/null && {
 	LOG 已启用 TCP 通道，执行 HTTP 保活
 	LOG 若保活失败，穿透通道可能需要在缩短心跳间隔后才稳定
 	stun_keep.sh &
 }
+
+# 连通性检测
+until echo | socat - $L4PROTO:$WANADDR:$WANPORT,connect-timeout=2 2>/dev/null; do
+	let STUN_CHECK++
+	[ $STUN_CHECK -ge 10 ] && break
+	sleep 1
+done
+if [ $STUN_CHECK ] && [ $STUN_CHECK -ge 10 ]; then
+	LOG $WANADDR:$WANPORT/$L4PROTO 连通性检测失败，请确认路径上的防火墙
+else
+	LOG $WANADDR:$WANPORT/$L4PROTO 连通性检测成功
+fi
