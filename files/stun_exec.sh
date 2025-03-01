6d38430982f50b2fa2ce0d70490dc264d13a31ed
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-03-01 23:07:32 +0800

    Update stun_exec.sh

--- files/stun_exec.sh
+++ files/stun_exec.sh
@@ -36,72 +36,13 @@ echo $WANPORT $LANPORT >StunPort_$L4PROTO
 }
 
 # UPnP
-[ "$StunUpnp" = 0 ] || {
-	LOG 已启用 UPnP
-	ADD_UPNP() {
-		[ $StunUpnpInterface ] && local StunUpnpInterface='-m '$StunUpnpInterface''
-		[ $StunUpnpUrl ] && local StunUpnpUrl='-u '$StunUpnpUrl''
-		[ $StunUpnpAddr ] || local StunUpnpAddr=@
-		UPNP_START='upnpc '$StunUpnpArgs' '$StunUpnpInterface' '$StunUpnpUrl' -i -e "STUN BitComet Docker" -a '$StunUpnpAddr' '$UPNP_INPORT' '$UPNP_EXPORT' '$L4PROTO''
-#		[ $UPNP_TRY ] || {
-#			LOG 本次 UPnP 执行命令
-#			LOG $UPNP_START
-#		}
-		UPNP_RES=$(eval $UPNP_START 2>&1)
-		UPNP_FLAG=$?
-		[ $UPNP_FLAG = 0 ] && LOG 更新 UPnP 规则成功
-	}
-	[ -f StunUpnpInterface ] && StunUpnpInterface='br-lan'
-	[[ $StunMode =~ nft ]] || {
-		UPNP_INPORT=$WANPORT
-		UPNP_EXPORT=$LANPORT
-	}
-	[[ $StunMode =~ nft ]] && {
-		UPNP_INPORT=$APPPORT
-		UPNP_EXPORT=$APPPORT
-	}
-	LOG 本次 UPnP 规则：转发 外部端口 $UPNP_EXPORT/$L4PROTO 至 内部端口 $UPNP_INPORT/$L4PROTO
-	ADD_UPNP
-	[ $UPNP_FLAG = 1 ] && [[ $UPNP_RES == *'No IGD UPnP Device found on the network'* ]] && [ "$StunUpnpInterface" != '-m br-lan' ] && [ $(ls /sys/class/net | grep ^br-lan$) ] && {
-		LOG 未找到 IGD UPnP 设备，尝试使用 br-lan 接口
-		StunUpnpInterface='br-lan'
-		ADD_UPNP
-		[[ $UPNP_FLAG =~ ^[02]$ ]] && echo br-lan >StunUpnpInterface
-	}
-	[ $UPNP_FLAG = 2 ] && [[ $UPNP_RES == *'ConflictWithOtherMechanisms'* ]] && \
-	awk '{print$2}' /proc/net/$L4PROTO /proc/net/${L4PROTO}6 | grep -qi ":$(printf '%04x' $LANPORT)" && \
-	([ ! -f StunUpnpConflict_$L4PROTO ] || [ $(($(date +%s)-$(stat -c %Y StunUpnpConflict_$L4PROTO))) -gt 3600 ]) && {
-		LOG IGD UPnP 设备启用了端口占用检测
-		>StunUpnpConflict_$L4PROTO
-		if pgrep -f stun_keep.sh >/dev/null; then
-			LOG 结束 HTTP 保活并等待端口释放，最大限时 300 秒
-			pkill -f stun_keep.sh
-			timeout 300 bash -c "while awk '{print\$2}' /proc/net/$L4PROTO | grep -qi ":$(printf '%04x' $UPNP_EXPORT)"; do sleep 1; done"
-			if [ $? = 0 ]; then
-				LOG 端口释放成功，继续尝试更新 UPnP 规则
-			else
-				LOG 端口释放失败，仍继续尝试更新 UPnP 规则
-			fi
-		else
-			LOG 请确认正在使用 $UPNP_EXPORT/$L4PROTO 的程序，仍继续尝试更新 UPnP 规则
-		fi
-		until [ $UPNP_FLAG = 0 ] || [ "$UPNP_TRY" = 5 ]; do
-			let UPNP_TRY++
-			LOG UPnP 第 $UPNP_TRY 次重试，最多 5 次
-			ADD_UPNP
-			[ $UPNP_FLAG = 0 ] || [ $UPNP_TRY = 5 ] || sleep 15
-		done
-		[ $UPNP_FLAG = 0 ] && rm -f StunUpnpConflict_$L4PROTO
-	}
-	[ $UPNP_FLAG = 1 ] && LOG 更新 UPnP 规则失败，错误信息如下 && LOG "$UPNP_RES" | head -1
-	[ $UPNP_FLAG = 2 ] && LOG 更新 UPnP 规则失败，错误信息如下 && LOG "$UPNP_RES" | tail -1
-}
+[[ $StunMode =~ nft ]] || [ "$StunUpnp" = 0 ] || stun_upnp.sh $WANPORT $LANPORT $L4PROTO
 
 # TCP 通道保活
 [ $L4PROTO = tcp ] && ! pgrep -f stun_keep.sh >/dev/null && {
 	LOG 已启用 TCP 通道，执行 HTTP 保活
 	LOG 若保活失败，穿透通道可能需要在缩短心跳间隔后才稳定
-	stun_keep.sh &
+	setsid stun_keep.sh &
 }
 
 # 连通性检测
