5def7e2ff77827b0c0c0e539160edbfe88a59fd6
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-03-03 23:05:36 +0800

    Update stun_upnp_check.sh

--- files/stun_upnp_check.sh
+++ files/stun_upnp_check.sh
@@ -14,10 +14,14 @@ pkill -Af $0.*$L4PROTO
 
 # 检测 UPnP 规则有效性
 while :; do
-	[ $STUN_UPNP_KEEP ] || sleep $(($StunInterval*10))
-	[ $STUN_UPNP_KEEP ] && sleep $(($StunInterval/$STUN_UPNP_KEEP))
-	echo | socat - $L4PROTO:$WANADDR:$WANPORT,connect-timeout=2 2>/dev/null || let STUN_UPNP_CHECK++
-	[ $STUN_UPNP_CHECK -ge 10 ] && {
+	[ $STUN_UPNP_CHECK ] || sleep $(($StunInterval*10))
+	[ $STUN_UPNP_CHECK ] && sleep $(($StunInterval/$STUN_UPNP_KEEP))
+	if echo | socat - $L4PROTO:$WANADDR:$WANPORT,connect-timeout=2 2>/dev/null; then
+		unset STUN_UPNP_CHECK
+	else
+		let STUN_UPNP_CHECK++
+	fi
+	[ "$STUN_UPNP_CHECK" = 10 ] && {
 		[[ $StunMode =~ nft ]] && stun_upnp.sh $APPPORT $APPPORT $L4PROTO
 		[[ $StunMode =~ nft ]] || stun_upnp.sh $WANPORT $LANPORT $L4PROTO
 	}
