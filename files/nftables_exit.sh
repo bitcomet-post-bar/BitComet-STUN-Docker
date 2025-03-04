4eefaa755ba44940a8073b05815ab058f08765f2
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2025-03-04 19:26:27 +0800

    Update nftables_exit.sh

--- files/nftables_exit.sh
+++ files/nftables_exit.sh
@@ -9,7 +9,7 @@ CLEANUP() {
 	for HANDLE in $(nft -as list chain ip STUN BTTR_UDP | grep \"$NFTNAME\" | awk '{print$NF}'); do nft delete rule ip STUN BTTR_UDP handle $HANDLE; done
 	for HANDLE in $(nft -as list chain ip STUN NAT_OUTPUT | grep $NFTNAME | awk '{print$NF}'); do nft delete rule ip STUN NAT_OUTPUT handle $HANDLE; done
 	for HANDLE in $(nft -as list chain ip STUN NAT_POSTROUTING | grep $NFTNAME | awk '{print$NF}'); do nft delete rule ip STUN NAT_POSTROUTING handle $HANDLE; done
-	[ $StunModeLite ] || nft list chain ip STUN NAT_OUTPUT | grep -q BTTR_HTTPS || nft delete set ip STUN BTTR_HTTPS
+	[ "$StunModeHttps" = 0 ] || nft list chain ip STUN NAT_OUTPUT | grep -q BTTR_HTTPS || nft delete set ip STUN BTTR_HTTPS
 	nft list chain ip STUN BTTR_HTTP | grep -qvE '[{}]$|policy accept' || nft delete chain ip STUN BTTR_HTTP
 	nft list chain ip STUN BTTR_UDP | grep -qvE '[{}]$|policy accept' ] || nft delete chain ip STUN BTTR_UDP
 	nft list chain ip STUN NAT_OUTPUT | grep -qvE '[{}]$|policy accept' || nft delete chain ip STUN NAT_OUTPUT
